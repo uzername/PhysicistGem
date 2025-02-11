def5bb548faa7c47b3f4d23938dcc3d74976099a
uzername <ivanpost777@gmail.com>
2025-02-11 23:16:53 +0200

    Initial commit

---
+++ Assets/Scripts/LaunchProjectileMy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchProjectileMy : MonoBehaviour
+{
+    public GameObject projectile;
+    public Transform launchPoint;
+    public float launchVelocity = 700f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (launchPoint == null)
+            {
+                Debug.Log("launch point not defined");
+            }
+            GameObject ball = Instantiate(projectile,
+            launchPoint.position, launchPoint.rotation);
+
+            ball.GetComponent<Rigidbody>().AddRelativeForce(new
+            Vector3(launchVelocity,0,0));
+        }
+    }
+}
