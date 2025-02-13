0b99d8d16c77a28e6958a78d6815058be9eba2f6
uzername <ivanpost777@gmail.com>
2025-02-13 23:15:05 +0200

    added starter asset character controller and camera and launch using button

--- Assets/Scripts/LaunchProjectileMy.cs
+++ Assets/Scripts/LaunchProjectileMy.cs
@@ -6,21 +6,27 @@ public class LaunchProjectileMy : MonoBehaviour
 {
     public GameObject projectile;
     public Transform launchPoint;
+
+    public bool launchOnClick = false;
     public float launchVelocity = 700f;
+    // launch angle and initial direction of launch is defined by launchPoint Position and Rotation
 
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && launchOnClick)
         {
-            if (launchPoint == null)
-            {
-                Debug.Log("launch point not defined");
-            }
-            GameObject ball = Instantiate(projectile,
-            launchPoint.position, launchPoint.rotation);
-
-            ball.GetComponent<Rigidbody>().AddRelativeForce(new
-            Vector3(launchVelocity,0,0));
+            performLaunching();
         }
+    }
+    public void performLaunching()
+    {
+        if (launchPoint == null)
+        {
+            Debug.Log("launch point not defined");
+        }
+        GameObject ball = Instantiate(projectile,
+        launchPoint.position, launchPoint.rotation);
+
+        ball.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(launchVelocity, 0, 0));
     }
 }
